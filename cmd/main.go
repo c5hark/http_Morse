7f0a8a31e0dbf5400f193f727549d4255c17e53a
Timur Davletshin <timurdavletschin@yandex.ru>
2025-05-26 22:32:35 +0500

    <first commit>

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "morse_server: ", log.LstdFlags|log.Lshortfile)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Server is running at http://localhost:8080")
+
+	err := srv.HTTP.ListenAndServe()
+	if err != nil {
+		logger.Fatal("server startup error", err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,59 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../index.html")
+}
+
+func MorseHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "form parsing error", http.StatusInternalServerError)
+		return
+	}
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "failed to retieve the file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "failed to read the file", http.StatusInternalServerError)
+		return
+	}
+
+	converted, err := service.TranslateToMorse(string(content))
+	if err != nil {
+		http.Error(w, "failed to convert the file", http.StatusInternalServerError)
+		return
+	}
+
+	fileName := time.Now().UTC().Format("20060102_150405") + filepath.Ext(handler.Filename)
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		http.Error(w, "failed to create the file", http.StatusInternalServerError)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = newFile.WriteString(converted)
+	if err != nil {
+		http.Error(w, "failed to write converted data to file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(w, "Результат:\n"+converted)
+}
